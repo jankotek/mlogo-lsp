947190b2e9c3dacaff27826ecf3131fdf6dce52f
Jan Kotek <jan@kotek.net>
2025-06-04 15:07:53 +0200

    Update MLogo file for unit tests * do not call System.exit() * pluggable System.out * some methods package protected to make it testable

--- src/main/java/com/bme/mlogo/MLogo.java
+++ src/main/java/com/bme/mlogo/MLogo.java
@@ -8,8 +8,14 @@ import static com.bme.logo.Primitives.*;
 public class MLogo {
 	static final String version = "MLogo 0.1";
 
+	//TODO make this class non static
+	static PrintStream out = System.out;
+
+	private static boolean callSystemExit = false;
+
 	public static void main(String[] a) {
 		List<String> args = new ArrayList<String>(Arrays.asList(a));
+		callSystemExit = true;
 
 		boolean printHelp   = args.size() == 0;
 		boolean interactive = false;
@@ -24,14 +30,14 @@ public class MLogo {
 		}
 
 		if (printHelp) {
-			System.out.println(version);
-			System.out.println("usage: MLogo [-hit] file ...");
-			System.out.println();
-			System.out.println(" h : print this help message");
-			System.out.println(" i : provide an interactive REPL session");
-			System.out.println(" t : enable turtle graphics during batch mode");
-			System.out.println(" T : enable execution trace");
-			System.out.println();
+			out.println(version);
+			out.println("usage: MLogo [-hit] file ...");
+			out.println();
+			out.println(" h : print this help message");
+			out.println(" i : provide an interactive REPL session");
+			out.println(" t : enable turtle graphics during batch mode");
+			out.println(" T : enable execution trace");
+			out.println();
 		}
 
 		Environment e = kernel();
@@ -43,7 +49,11 @@ public class MLogo {
 			TurtleGraphics t = new TurtleGraphics(e);
 			for(String fileName : args) { runFile(e, fileName, t); }
 			if (interactive) { repl(e, t); }
-			else { System.exit(0); }
+			else {
+				if(callSystemExit)
+					System.exit(0);
+				return;
+			}
 		}
 		else {
 			for(String fileName : args) { runFile(e, fileName, null); }
@@ -55,33 +65,34 @@ public class MLogo {
 	}
 
 	private static void repl(Environment env, TurtleGraphics t) {
-		System.out.println(version);
-		System.out.println("type 'exit' to quit.");
-		System.out.println();
+		out.println(version);
+		out.println("type 'exit' to quit.");
+		out.println();
 		Scanner in = new Scanner(System.in);
 
 		while(true) {
-			System.out.print(">");
+			out.print(">");
 			try {
 				String line = in.nextLine();
 				if ("exit".equals(line)) { break; }
 				while(Parser.complete(line).size() > 0) {
-					System.out.print(">>");
+					out.print(">>");
 					line += "\n" + in.nextLine();
 				}
 				runString(env, line, t);
 			}
 			catch(SyntaxError e) {
-				System.out.format("syntax error: %s%n", e.getMessage());
-				System.out.format("\t%s%n\t", e.line);
+				out.format("syntax error: %s%n", e.getMessage());
+				out.format("\t%s%n\t", e.line);
 				for(int z = 0; z < e.lineIndex; z++) {
-					System.out.print(e.line.charAt(z) == '\t' ? '\t' : ' ');
+					out.print(e.line.charAt(z) == '\t' ? '\t' : ' ');
 				}
-				System.out.println("^");
+				out.println("^");
 				env.reset();
 			}
 		}
-		System.exit(0);
+		if(callSystemExit)
+			System.exit(0);
 	}
 
 	private static void runString(Environment env, String sourceText, TurtleGraphics t) {
@@ -100,16 +111,16 @@ public class MLogo {
 			}
 		}
 		catch(RuntimeError e) {
-			System.out.format("runtime error: %s%n", e.getMessage());
+			out.format("runtime error: %s%n", e.getMessage());
 			//e.printStackTrace();
 			for(LAtom atom : e.trace) {
-				System.out.format("\tin %s%n", atom);
+				out.format("\tin %s%n", atom);
 			}
 			env.reset();
 		}
 	}
 
-	private static void runFile(Environment env, String filename, TurtleGraphics t) {
+	static void runFile(Environment env, String filename, TurtleGraphics t) {
 		try {
 			LList code = Parser.parse(loadFile(filename));
 			if (t == null) {
@@ -129,20 +140,22 @@ public class MLogo {
 			}
 		}
 		catch(SyntaxError e) {
-			System.out.format("%d: syntax error: %s%n", e.lineNumber, e.getMessage());
-			System.out.format("\t%s%n\t", e.line);
+			out.format("%d: syntax error: %s%n", e.lineNumber, e.getMessage());
+			out.format("\t%s%n\t", e.line);
 			for(int z = 0; z < e.lineIndex; z++) {
-				System.out.print(e.line.charAt(z) == '\t' ? '\t' : ' ');
+				out.print(e.line.charAt(z) == '\t' ? '\t' : ' ');
 			}
-			System.out.println("^");
-			System.exit(1);
+			out.println("^");
+			if(callSystemExit)
+				System.exit(1);
 		}
 		catch(RuntimeError e) {
-			System.out.format("runtime error: %s%n", e.getMessage());
+			out.format("runtime error: %s%n", e.getMessage());
 			for(LAtom atom : e.trace) {
-				System.out.format("\tin %s%n", atom);
+				out.format("\tin %s%n", atom);
 			}
-			System.exit(1);
+			if(callSystemExit)
+				System.exit(1);
 		}
 	}
 
@@ -160,50 +173,51 @@ public class MLogo {
 			return ret.toString();
 		}
 		catch(IOException e) {
-			System.err.format("Unable to load file '%s'.%n", filename);
-			System.exit(1);
+			out.format("Unable to load file '%s'.%n", filename);
+			if(callSystemExit)
+				System.exit(1);
 			return null;
 		}
 	}
 
-	private static void primitiveIO(Environment e, boolean trace) {
+	static void primitiveIO(Environment e, boolean trace) {
 		final LWord a = new LWord(LWord.Type.Name, "argument1");
 		final Scanner in = new Scanner(System.in);
 
 		if (trace) {
 			e.addTracer(new Tracer() {
-				public void begin()  { System.out.println("tracer: begin."); }
-				public void end()    { System.out.println("tracer: end.");   }
-				//public void tick() { System.out.println("tracer: tick.");  }
+				public void begin()  { out.println("tracer: begin."); }
+				public void end()    { out.println("tracer: end.");   }
+				//public void tick() { out.println("tracer: tick.");  }
 
 				public void callPrimitive(String name, Map<LAtom, LAtom> args) {
-					System.out.format("trace: PRIM %s%s%n",
+					out.format("trace: PRIM %s%s%n",
 						name,
 						args.size() > 0 ? " " + args : ""
 					);
 				}
 				public void call(String name, Map<LAtom, LAtom> args, boolean tail) {
-					System.out.format("trace: CALL %s%s%s%n",
+					out.format("trace: CALL %s%s%s%n",
 						name,
 						args.size() > 0 ? " " + args : "",
 						tail ? " (tail)" : ""
 					);
 				}
 				public void output(String name, LAtom val, boolean implicit) {
-					System.out.format("trace: RETURN %s- %s%s%n", name, val, implicit ? " (implicit)" : "");
+					out.format("trace: RETURN %s- %s%s%n", name, val, implicit ? " (implicit)" : "");
 				}
 				public void stop(String name, boolean implicit) {
-					System.out.format("trace: STOP %s%s%n", name, implicit ? " (implicit)" : "");
+					out.format("trace: STOP %s%s%n", name, implicit ? " (implicit)" : "");
 				}
 				public void define(String name) {
-					System.out.format("trace: DEFINE %s%n", name);
+					out.format("trace: DEFINE %s%n", name);
 				}
 			});
 		}
 
 		e.bind(new LWord(LWord.Type.Prim, "version") {
 			public void eval(Environment e) {
-				System.out.println(MLogo.version);
+				out.println(MLogo.version);
 			}
 		});
 
@@ -211,9 +225,9 @@ public class MLogo {
 			public void eval(Environment e) {
 				List<LWord> words = new ArrayList<LWord>(e.words());
 				Collections.sort(words);
-				for(LWord word : words) { System.out.print(word + " "); }
-				System.out.println();
-				System.out.println();
+				for(LWord word : words) { out.print(word + " "); }
+				out.println();
+				out.println();
 			}
 		});
 
@@ -230,23 +244,23 @@ public class MLogo {
 
 		e.bind(new LWord(LWord.Type.Prim, "trace") {
 			public void eval(Environment e) {
-				System.out.println("trace: ");
+				out.println("trace: ");
 				for(LAtom s : e.trace()) {
-					System.out.println("\t" + s);
+					out.println("\t" + s);
 				}
-				System.out.println();
+				out.println();
 			}
 		});
 
 		e.bind(new LWord(LWord.Type.Prim, "print") {
 			public void eval(Environment e) {
-				System.out.println(e.thing(a));
+				out.println(e.thing(a));
 			}
 		}, a);
 
 		e.bind(new LWord(LWord.Type.Prim, "println") {
 			public void eval(Environment e) {
-				System.out.println();
+				out.println();
 			}
 		});
 
