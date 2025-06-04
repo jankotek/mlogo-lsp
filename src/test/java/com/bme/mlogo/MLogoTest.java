720da4a41ca76704be9d5ba70b8a53684d077067
Jan Kotek <jan@kotek.net>
2025-06-04 14:52:02 +0200

    mavenize, migrate to unit tests, change System.out for unit tests

---
+++ src/test/java/com/bme/mlogo/MLogoTest.java
@@ -0,0 +1,54 @@
+package com.bme.mlogo;
+
+import com.bme.logo.Environment;
+import com.bme.logo.Primitives;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MLogoTest {
+
+    @Test
+    public void testAndCompareOutputs() throws IOException {
+        File testDataDir = new File("./test");
+        assertTrue(testDataDir.exists() && testDataDir.isDirectory(), "test data dir");
+
+        for(File outF : testDataDir.listFiles() ){
+            if(!outF.getName().endsWith(".logo"))
+                continue;
+
+            Path expectedOutputFile = Path.of(outF.getPath().replaceAll("logo$", "out"));
+            Path expectedErrFile = Path.of(outF.getPath().replaceAll("logo$", "err"));
+
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            MLogo.out = new PrintStream(out);
+
+            Environment e = Primitives.kernel();
+            MLogo.primitiveIO(e, false);
+
+//            TurtleGraphics t = new TurtleGraphics(e);
+            MLogo.runFile(e, outF.getPath(), null);
+            MLogo.out.close();
+
+            //TODO catch failed exit code?!
+
+            String testOutput = out.toString("UTF-8");
+            Path eOutF = Files.exists(expectedOutputFile)? expectedOutputFile: expectedErrFile;
+            String expectedOutput = Files.readString(eOutF);
+            assertEquals(expectedOutput, testOutput, outF.getName());
+
+        }
+
+    }
+    @AfterEach
+    public void cleanup(){
+        MLogo.out = System.out;
+    }
+}
